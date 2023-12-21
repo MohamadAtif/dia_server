e180dea0eb9a0e1a302a92b122c073e217e2e302
MohamadAtif <137002849+MohamadAtif@users.noreply.github.com>
2023-12-21 02:58:27 +0200

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,80 @@
+const express=require('express');
+const User = require('../models/user');
+const bcryptJs=require('bcryptjs');
+const authRouter=express.Router();
+const jwt=require('jsonwebtoken');
+const auth = require('../middlewares/auth');
+
+
+ authRouter.post('/api/signup',async (req,res)=>{
+   try {
+      const{name,email,password,phone}=req.body;
+      const exitingUser = await User.findOne({email}); 
+    if(exitingUser){
+     return res.status(400).json({msg:'User Already Exists!'})
+    }
+    const hashedPassword= await bcryptJs.hash(password,8)
+    let user=new User({
+       email ,
+       password:hashedPassword,
+       name,
+       phone
+    })
+    user=await user.save();
+    res.json({user});
+   } catch (e) {
+      res.status(500).json({
+         error:e.message
+      })
+   }
+ }
+)
+
+authRouter.post('/api/signin',async (req,res)=>{
+   try {
+      const{email,password}=req.body;
+      const user = await User.findOne({email}); 
+    if(!user){
+     return res.status(400).json({msg:'User with this Email does not Exist!'})
+    }
+    const isMatch= await bcryptJs.compare(password,user.password)
+    if(!isMatch){
+      return res.status(400).json({msg:'Incorrect Password!'})
+    }
+   const token= jwt.sign({id:user._id},"passwordKey");
+  res.json({token,...user._doc});
+
+   } catch (e) {
+      res.status(500).json({
+         error:e.message
+      })
+   }
+ }
+)
+authRouter.post('/tokenIsValid',async (req,res)=>{
+   try {
+      const token =req.header('x-auth-token');
+           if(!token)res.json(false);
+     const verified = jwt.verify(token,'passwordKey')
+     if(!verified) res.json(false);
+     const user=await User.findById(verified.id);
+     if(!user)res.json(false);
+     res.json(true);
+
+   } catch (e) {
+      res.status(500).json({
+         error:e.message
+      })
+   }
+ }
+)
+
+authRouter.get('/',auth, async(req,res)=>{
+const user =await User.findById(req.user);
+res.json({...user._doc , token:req.token})
+}
+
+)
+
+module.exports=authRouter;
+
